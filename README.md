81a3bb5f54fc6acb98a7a95fe62d3e2e17004757
Kontur <sequence@myrealm.ca>
2021-09-18 04:37:09 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# xkcd.com random comics to VK posting utility
+
+A Python utility that uses the API of [xkcd.com](xkcd.com) to download a random image and caption from the same comics website, and the API of [vk.com](vk.com) to upload and publish the downloaded image to the same social network. The utility deletes the downloaded image locally.
+
+## Installation
+
+1. Python 3 should already be installed. If not, then please do so.
+2. You also need to have a [VK](www.vk.com) account. You also need to [create](https://vk.com/groups?tab=admin) a VK group in which you will be posting the comics with this utility.
+3. Please refer to [here](https://vk.com/dev) to create an app in VK, which you will use for interacting with the VK API to post the comics (go to the "My apps" subsection there). Your app will have its own unique `client_id`, which you can see in your browser's address bar after click the "Edit" button beside your newly created VK app. Write this `client_id` down in a safe place.
+4. Get your API user access key with the following access rights: `photos`, `groups`, `wall` and `offline`. It looks something like `533bacf01e1165b57531ad114461ae8736d6506a3`, and it also appears in your browser's address bar after the `access_token=` part of the address. Your access key request URL should look like this:
+
+    https://oauth.vk.com/authorize?client_id=1234567&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=photos,groups,wall,offline&response_type=token&v=5.131
+
+    (make sure to substitute your `client_id` there).
+
+5. You can get your VK User ID and Group ID [here](https://regvk.com/id/).
+
+6. Now create a Python virtual environment:
+
+    python3 -m venv .venv
+
+    Activate the environment, e.g.:
+
+        source .venv/bin/activate
+
+    Install the required libraries:
+
+        pip install -r requirements.txt
+
+7. Now you need to set up the utility by creating a file named `.env` in the utility's directory.
+
+> Example of `.env`:
+
+    VK_CLIENT_ID = 1234567
+    VK_ACCESS_TOKEN = "533bacf01e1165b57531ad114461ae8736d6506a3"
+    VK_UID = "658704571"
+    VK_GID = "206559520"
+    VK_API_VER = "5.131"
+
+> **Note:** If you deploy this script on a service which does not use a `.env` (or similar) file, but which would store these environment variables in its own environment (such as [Heroku](http://heroku.com)), you don't need to change anything in the code. The above environment variables will be picked up automatically from the current environment if no `.env` file is present.
+
+## Launching the program
+
+If you are in Linux and make the file executable by
+
+    chmod +x main.py
+
+then you can also launch directly, e.g.:
+
+    ./main.py
---
+++ main.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+from dotenv import load_dotenv
+from os import getenv, remove
+import json
+import logging
+from pathlib import Path
+from os import path
+from random import randint
+import requests
+import urllib3
+
+
+def fetch_image(img_url, file_name, folder='.'):
+    file_path = path.join(folder, file_name)
+    if Path(file_path).is_file():
+        logging.info(
+            f"fetch_image(): {file_path} already exists, not fetching it"
+        )
+        return file_path
+
+    img_response = requests.get(img_url, verify=False)
+    img_response.raise_for_status()
+    with open(file_path, 'wb') as file:
+        file.write(img_response.content)
+    return file_path
+
+
+def get_photo_publish_address(vk_access_token, vk_group_id, vk_api_ver):
+    photo_upload_api_url = "https://api.vk.com/method/photos.getWallUploadServer"
+    photo_upload_params = {"access_token": vk_access_token,
+                           "group_id": vk_group_id, "v": vk_api_ver}
+    photo_upload_url_response = requests.get(photo_upload_api_url,
+                                             params=photo_upload_params)
+    photo_upload_url_response.raise_for_status()
+    return photo_upload_url_response.json()['response']['upload_url']
+
+
+def upload_photo_vk(file_path, photo_upload_addr):
+    with open(file_path, 'rb') as file:
+        files = {
+            'file1': file,
+        }
+        response = requests.post(photo_upload_addr, files=files)
+        response.raise_for_status()
+        return response.json()
+
+
+def save_photo_vk(vk_access_token, vk_group_id, vk_api_ver, vk_photo_param,
+                  vk_server_param, vk_hash, photo_caption):
+    photo_save_api_url = "https://api.vk.com/method/photos.saveWallPhoto"
+    photo_save_params = {"access_token": vk_access_token,
+                         "v": vk_api_ver, "group_id": vk_group_id,
+                         "photo": vk_photo_param, "server": vk_server_param,
+                         "hash": vk_hash, "caption": photo_caption}
+    photo_save_response = requests.post(photo_save_api_url,
+                                        params=photo_save_params)
+    photo_save_response.raise_for_status()
+    return photo_save_response.json()
+
+
+def publish_photo_vk(vk_access_token, vk_api_ver, owner_id, from_group,
+                     message, photo_owner_id, photo_media_id):
+    photo_publish_api_url = "https://api.vk.com/method/wall.post"
+    photo_publish_params = {"access_token": vk_access_token,
+                            "v": vk_api_ver, "owner_id": -int(owner_id),
+                            "message": message, "from_group": from_group,
+                            "attachments":
+                                [f"photo{photo_owner_id}_{photo_media_id}"]}
+    photo_publish_reponse = requests.post(photo_publish_api_url,
+                                          params=photo_publish_params)
+    photo_publish_reponse.raise_for_status()
+    return photo_publish_reponse.json()
+
+
+def post_photo_vk(vk_access_token, vk_group_id, vk_api_ver, img_file_name,
+                  comic_comment):
+    photo_upload_addr = get_photo_publish_address(vk_access_token,
+                                                  vk_group_id, vk_api_ver)
+    vk_photo_upload_response = upload_photo_vk(img_file_name,
+                                               photo_upload_addr)
+    vk_save_photo_response = save_photo_vk(vk_access_token, vk_group_id,
+                                           vk_api_ver,
+                                           vk_photo_upload_response["photo"],
+                                           vk_photo_upload_response["server"],
+                                           vk_photo_upload_response["hash"],
+                                           comic_comment)
+    vk_publish_photo = publish_photo_vk(
+        vk_access_token, vk_api_ver, vk_group_id, 1,
+        vk_save_photo_response["response"][0]["text"],
+        vk_save_photo_response["response"][0]["owner_id"],
+        vk_save_photo_response["response"][0]["id"]
+        )
+    return vk_publish_photo
+
+
+def post_comic_vk(vk_access_token, vk_group_id, vk_api_ver,
+                  latest_comic_number=0, comic_response=None):
+
+    if comic_response is None:
+        random_comic_number = randint(0, latest_comic_number)
+        comic_response = requests.get(
+            f"https://xkcd.com/{random_comic_number}/info.0.json"
+            )
+        comic_response.raise_for_status()
+        comic_response = json.loads(comic_response.text)
+    comic_img_url = comic_response["img"]
+    comic_comment = comic_response["alt"]
+    img_file_name = comic_img_url.split("/")[-1]
+    fetch_image(comic_img_url, img_file_name)
+    vk_response = post_photo_vk(vk_access_token, vk_group_id, vk_api_ver,
+                                img_file_name, comic_comment)
+    remove(img_file_name)
+    return vk_response
+
+
+def main():
+    load_dotenv()
+    vk_access_token = getenv("VK_ACCESS_TOKEN")
+    vk_api_ver = getenv("VK_API_VER")
+    vk_group_id = getenv("VK_GID")
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    comic_response = requests.get("https://xkcd.com/info.0.json")
+    comic_response.raise_for_status()
+    comic_response = json.loads(comic_response.text)
+    latest_comic_number = comic_response["num"]
+
+    # Post a random comic
+    post_comic_vk(vk_access_token, vk_group_id, vk_api_ver,
+                  latest_comic_number)
+
+
+if __name__ == '__main__':
+    main()
